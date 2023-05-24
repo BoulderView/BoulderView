14cdd36815747f0dbd8d0daa636472d154fb8eb0
leezhengjing <zhengjing.lee86@gmail.com>
2023-05-24 13:10:15 +0800

    HomeScreen - created dummy data list
    changed from scrollview to flatlist

--- screens/bottomTabNavigation/HomeScreen.tsx
+++ screens/bottomTabNavigation/HomeScreen.tsx
@@ -1,11 +1,17 @@
 import * as React from 'react';
-import { Text, View, StyleSheet, ScrollView } from "react-native";
+import { Text, View, StyleSheet, ScrollView, FlatList } from "react-native";
 import { Button, Card, Paragraph, Title } from "react-native-paper";
 
 import { SearchBar } from '../../components/SearchBar';
 import { HomeCard } from '../../components/HomeCard';
 
-
+const GYM_DATA = [
+  { title: "Boruda", coverImage: require("../../assets/borudaclimbing.jpg"), content: "boruda - a concept boulder gym inspired by the Japanese climbing scene." },
+  { title: "Lighthouse Climbing", coverImage: require("../../assets/lighthouse.jpg"), content: "Opened in August 2020, Lighthouse is Singapore’s first crowdfunded climbing gym." },
+  { title: "Fitbloc", coverImage: require("../../assets/fitbloc2.png"), content: "Singapore's largest indoor fitness and bouldering gym, equipped with 3 rock climbing walls, a weights room, studio and a swimming pool." },
+  { title: "BoulderPlanet - Tai Seng", coverImage: require("../../assets/boulderplanet.png"), content: "At Boulder Planet, we live by a simple motto: Nothing Out Of Reach." },
+  { title: "Boulder+ Chevrons", coverImage: require("../../assets/boulderplus.jpg"), content: "Aperia Mall is home to boulder+, Singapore's newest bouldering gym. Come see what the fuss is about." },
+]
 
 export const HomeScreen = () => {
   return (
@@ -15,13 +21,13 @@ export const HomeScreen = () => {
         <Button>Locations</Button>
         <Button>Favourited Places</Button>
       </View>
-      <ScrollView>
-        <HomeCard title="Boruda" coverImage={require('../../assets/borudaclimbing.jpg')} content='boruda - a concept boulder gym inspired by the Japanese climbing scene.' ></HomeCard>
-        <HomeCard title="Lighthouse Climbing" coverImage={require("../../assets/lighthouse.jpg")} content="Opened in August 2020, Lighthouse is Singapore’s first crowdfunded climbing gym."></HomeCard>
-        <HomeCard title="Fitbloc" coverImage={require("../../assets/fitbloc2.png")} content="Singapore's largest indoor fitness and bouldering gym, equipped with 3 rock climbing walls, a weights room, studio and a swimming pool."></HomeCard>
-        <HomeCard title="Boulder Planet - Tai Seng" coverImage={require('../../assets/boulderplanet.png')} content='At Boulder Planet, we live by a simple motto: Nothing Out Of Reach.'></HomeCard>
-        <HomeCard title="Boulder+ Chevrons" coverImage={require('../../assets/boulderplus.jpg')} content="Aperia Mall is home to boulder+, Singapore's newest bouldering gym. Come see what the fuss is about."></HomeCard>
-      </ScrollView>
+      <FlatList
+        data={GYM_DATA}
+        renderItem={({ item }) =>
+          <HomeCard title={item.title}
+            coverImage={item.coverImage}
+            content={item.content}></HomeCard>}
+      />
     </View >
   );
 }
