810c5e3ff3de30a32d30f9bbcf1231eae971ae1c
kurtyjlee <kurtyjlee@gmail.com>
2023-06-04 12:37:16 +0800

    take pic

--- app/(tabs)/post/index.tsx
+++ app/(tabs)/post/index.tsx
@@ -1,42 +1,94 @@
 import * as React from 'react';
-import { Text, View, StyleSheet, TouchableOpacity, Button } from "react-native";
-import { Camera, CameraType } from 'expo-camera';
-import { useState } from 'react';
+import { Text, View, StyleSheet, Image } from "react-native";
+import { Camera, CameraCapturedPicture, CameraType } from 'expo-camera';
+import { useRef, useState } from 'react';
+import { Button } from 'react-native-paper';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 const PostScreen= () => {
+  let cameraRef = useRef<Camera | null>(null);
   const [type, setType] = useState(CameraType.back);
   const [permission, requestPermission] = Camera.useCameraPermissions();
+  const [photo, setPhoto] = useState<CameraCapturedPicture | undefined>();
 
+  // Camera permissions are still loading
   if (!permission) {
-    // Camera permissions are still loading
     return <View />;
   }
 
+  // Camera permissions are not granted yet
   if (!permission.granted) {
-    // Camera permissions are not granted yet
     return (
       <View style={styles.container}>
         <Text style={{ textAlign: 'center' }}>We need your permission to show the camera</Text>
-        <Button onPress={requestPermission} title="grant permission" />
+        <Button mode="contained" onPress={requestPermission}>
+          Grant permission
+        </Button>
       </View>
     );
   }
 
+  // Change between front and back camera
   const toggleCameraType = () => {
-    setType(current => (current === CameraType.back ? CameraType.front : CameraType.back));
+    setType(current => 
+      (current === CameraType.back 
+        ? CameraType.front 
+        : CameraType.back));
     console.log(type);
   }
 
+  
+
+  // handles taking pictures
+  const takePic = async () => {
+    /*
+      Options for the pictures 
+      quality: 1 for best quality pictures
+      exif: remove shutter speed and other camera options
+    */
+    let options = {
+      quality:1,
+      base64:true,
+      exif:false
+    };
+
+    let newPhoto = await cameraRef.current?.takePictureAsync(options);
+    setPhoto(newPhoto);
+    console.log("photo taken");
+  }
+
+  // Preview the photo
+  if (photo) {
+    return (
+      <SafeAreaView style={styles.container}>
+        <Image style={styles.preview} source={{ uri: "data:image/jpg;base64," + photo.base64 }} />
+        <Button mode="contained" style={styles.button} onPress={() => {
+          setPhoto(undefined);
+          console.log("closing photo")
+        }}>
+          Close
+        </Button>
+      </SafeAreaView>
+    );
+  }
+
   return (
-    <View style={styles.container}>
-      <Camera style={styles.camera} type={type}>
+    <SafeAreaView style={styles.container}>
+      <Camera 
+        style={styles.camera} 
+        type={type}
+        ref={cameraRef}
+      >
         <View style={styles.buttonContainer}>
-          <TouchableOpacity style={styles.button} onPress={toggleCameraType}>
-            <Text style={styles.text}>Flip Camera</Text>
-          </TouchableOpacity>
+          <Button mode="contained" style={styles.button} onPress={toggleCameraType}>
+            Flip Camera
+          </Button>
+          <Button mode="contained" style={styles.button} onPress={takePic}>
+            Take picture
+          </Button>
         </View>
       </Camera>
-    </View>
+    </SafeAreaView>
   );
 };
 
@@ -45,28 +97,24 @@ export default PostScreen;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
   camera: {
     flex:1,
-    width:"100%"
   },
   buttonContainer: {
     flex: 1,
-    flexDirection: 'row',
     backgroundColor: 'transparent',
-    margin: 64,
+    alignItems:"center",
+    justifyContent:"flex-end",
   },
   button: {
-    flex: 1,
+    flex: 0.05,
     alignSelf: 'flex-end',
     alignItems: 'center',
+    margin:5,
   },
-  text: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    color: 'white',
-  },
+  preview: {
+    alignSelf:"stretch",
+    flex:1
+  }
 });
