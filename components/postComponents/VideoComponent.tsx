d379b65868730641bf9c352efc8692f9f531ab10
kurtyjlee <kurtyjlee@gmail.com>
2023-06-25 02:34:01 +0800

    video pauses when not in frame

--- components/postComponents/VideoComponent.tsx
+++ components/postComponents/VideoComponent.tsx
@@ -1,48 +1,49 @@
 import React, { useEffect, useRef } from 'react';
 import { StyleSheet, View, Dimensions } from 'react-native';
 import { ResizeMode, Video } from 'expo-av';
+import { useHeaderHeight } from '@react-navigation/elements';
+import { useBottomTabBarHeight } from '@react-navigation/bottom-tabs';
 
 interface Props {
-  videoLink:string
+  videoLink:string,
+  visibleVideoKey: string | null,
+  videoKey: string
 }
 
-const VideoComponent:React.FC<Props> = ({ videoLink }) => {
+const VideoComponent:React.FC<Props> = ({ videoLink, visibleVideoKey, videoKey }) => {
   
-    const videoRef = useRef(null);
+  const videoRef = useRef<Video | null>(null);
 
-    useEffect(() => {
-      console.log(videoLink);
-    }, [])
-  
-    return (
-      <View style={styles.container}>
-        <Video
-          ref={videoRef}
-          style={styles.preview}
-          source={{uri: videoLink}}
-          useNativeControls
-          resizeMode={ResizeMode.CONTAIN}
-          isLooping
-          shouldPlay
-        />
-      </View>
-    )
-  }
-  
-  export default VideoComponent;
-  
-  const styles = StyleSheet.create({
-    preview: {
-      width:"100%",
-      height:"100%",
-    },
-    container: {
-      height: Dimensions.get("window").height - 100,
-      width: Dimensions.get("window").width,
-      justifyContent: "flex-start",
-      alignItems:"flex-start",
-      alignSelf:"flex-start",
+  useEffect(() => {
+    if (visibleVideoKey === videoKey) {
+      videoRef.current?.playAsync();
+    } else {
+      videoRef.current?.pauseAsync();
     }
-  })
-  
+  }, [visibleVideoKey]);
 
+  return (
+    <View style={{
+      height: Dimensions.get("window").height - useBottomTabBarHeight() - useHeaderHeight(),
+      width: Dimensions.get("window").width,
+    }}>
+      <Video
+        ref={videoRef}
+        style={styles.preview}
+        source={{uri: videoLink}}
+        useNativeControls
+        resizeMode={ResizeMode.CONTAIN}
+        isLooping
+      />
+    </View>
+  )
+}
+  
+export default VideoComponent;
+
+const styles = StyleSheet.create({
+  preview: {
+    width:"100%",
+    height:"100%",
+  }
+})
